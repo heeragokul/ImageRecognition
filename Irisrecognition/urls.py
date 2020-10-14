9a6ab89ca7f2ab2e9c457f67174ab985ea8348f0
heera <heera1191@gmail.com>
2020-10-14 21:42:37 +0530

    url changes

--- Irisrecognition/urls.py
+++ Irisrecognition/urls.py
@@ -5,6 +5,6 @@ from Irisrecognition.views import TestView, ImageParser
 urlpatterns = [
 
     # path('', HomeView.as_view(), name='homeview'),
-    path('test', TestView.as_view(), name='test'),
+    path('', TestView.as_view(), name='test'),
     path('img', ImageParser.as_view(), name='image'),
 ]
